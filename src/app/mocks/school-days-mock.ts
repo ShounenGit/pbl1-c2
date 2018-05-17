15b8b4f32ebf237554fe5c36e188c5980a11c4e7
ponsea <puodsea@gmail.com>
2018-05-17 13:48:48 +0900

    登校日データのモック追加

---
+++ src/app/mocks/school-days-mock.ts
@@ -0,0 +1,16 @@
+export const SCHOOL_DAYS_MOCK = {
+  schoolDays: [
+    {
+      "date": "2018-05-01",
+      "periodCount": 5
+    },
+    {
+      "date": "2018-05-01",
+      "periodCount": 5
+    },
+    {
+      "date": "2018-05-02",
+      "periodCount": 5
+    }
+  ]
+}
