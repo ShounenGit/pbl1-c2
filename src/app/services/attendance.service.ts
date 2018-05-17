d37d51ceb82e1626439daf7c32f8a3027f77fdb2
ponsea <puodsea@gmail.com>
2018-05-17 13:50:42 +0900

    AttendanceService#getSchoolDaysのスタブ追加

--- src/app/services/attendance.service.ts
+++ src/app/services/attendance.service.ts
@@ -4,17 +4,27 @@ import 'rxjs/add/observable/of';
 import 'rxjs/add/operator/map';
 
 import { StudentWithAttendances } from '../models/student-with-attendances';
+import { SchoolDay } from '../models/school-day';
 import { USERS_ATTENDANCES_MOCK } from '../mocks/students-attendances-mock';
+import { SCHOOL_DAYS_MOCK } from '../mocks/school-days-mock';
 
 @Injectable()
 export class AttendanceService {
+  readonly statusNames = [
+    '出席', '遅刻', '欠席', '就活', '病欠', '公欠'
+  ];
 
-  constructor() {
-  }
+  readonly statusShortNames = this.statusNames.map(name => name[0]);
 
-  getStudents(until = new Date(), limit = 5): Observable<StudentWithAttendances[]> {
+  constructor() { }
+
+  getStudents(until = new Date(), limit = 3): Observable<StudentWithAttendances[]> {
     // TODO: implements API call to /students/attendances
     return Observable.of(USERS_ATTENDANCES_MOCK).map(res => res.students);
   }
 
+  getSchoolDays(until = new Date(), limit = 3): Observable<SchoolDay[]> {
+    // TODO: implements API call to /school_days
+    return Observable.of(SCHOOL_DAYS_MOCK).map(v => v.schoolDays);
+  }
 }
