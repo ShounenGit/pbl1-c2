f53864a182cc7ee2f8bacd892662863ef4b451fd
ShounenGit <kd1188449@st.kobedenshi.ac.jp>
2018-05-28 14:47:51 +0900

    bootstrapのtableを反映させた、日付選択フォームの追加

--- src/app/attendance-rate-confirmation/attendance-rate-confirmation.component.ts
+++ src/app/attendance-rate-confirmation/attendance-rate-confirmation.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { STUDENT_SCHOOL_DAYS_MOCK } from '../mocks/student-school-days-mock';
 import {StudentWithSchoolDays} from '../models/student-with-school-days';
+import {AttendanceService} from '../services/attendance.service';
 
 @Component({
   selector: 'app-attendance-rate-confirmation',
@@ -9,9 +10,27 @@ import {StudentWithSchoolDays} from '../models/student-with-school-days';
 })
 export class AttendanceRateConfirmationComponent implements OnInit {
   student = STUDENT_SCHOOL_DAYS_MOCK;
-  constructor() { }
+  AttendanceRate: number = 85.4;
+  selectDate = this.getDateStr(new Date());
+  getDateStr(dt: Date): string {
+    var str = dt.getFullYear().toString();
+    str = str + '-' + this.zeroPadding((dt.getMonth() + 1), 2);
+    str = str + '-' + this.zeroPadding(dt.getDate(), 2);
+    return str;
+  }
+  zeroPadding(n: number, d: number){
+    return (Array(d).join('0') + n).slice(-d);
+  }
+  constructor(private attendanceService: AttendanceService) {
+  }
 
   ngOnInit() {
   }
 
+  attendanceresult(status: number) {
+    return this.attendanceService.statusShortNames[status];
+  }
 }
+
+
+
--- src/app/mocks/student-school-days-mock.ts
+++ src/app/mocks/student-school-days-mock.ts
@@ -6,7 +6,7 @@ export const STUDENT_SCHOOL_DAYS_MOCK: StudentWithSchoolDays = {
   name: '山田太郎',
   schoolDays: [
     {
-     date: '2018-05-10',
+     date: '2018-05-11',
      attendances: [
        {
          id: 1,
@@ -14,44 +14,244 @@ export const STUDENT_SCHOOL_DAYS_MOCK: StudentWithSchoolDays = {
        },
        {
          id: 2,
-         status: 1,
+         status: 0,
        },
         {
           id: 3,
-         status: 1,
+         status: 0,
        },
        {
          id: 4,
-         status: 1,
+         status: 0,
        },
        {
          id: 5,
-         status: 1,
+         status: 0,
        }
       ]
     },
     {
-      date: '2018-05-11',
+      date: '2018-05-12',
       attendances: [
         {
           id: 1,
-          status: 1,
+          status: 0,
         },
         {
           id: 2,
-          status: 1,
+          status: 0,
         },
         {
           id: 3,
-          status: 1,
+          status: 0,
         },
         {
           id: 4,
-          status: 1,
+          status: 0,
         },
         {
           id: 5,
-          status: 1,
+          status: 0,
+        }
+      ]
+    },
+    {
+      date: '2018-05-13',
+      attendances: [
+        {
+          id: 1,
+          status: 0,
+        },
+        {
+          id: 2,
+          status: 0,
+        },
+        {
+          id: 3,
+          status: 0,
+        },
+        {
+          id: 4,
+          status: 0,
+        },
+        {
+          id: 5,
+          status: 0,
+        }
+      ]
+    },
+    {
+      date: '2018-05-14',
+      attendances: [
+        {
+          id: 1,
+          status: 0,
+        },
+        {
+          id: 2,
+          status: 0,
+        },
+        {
+          id: 3,
+          status: 0,
+        },
+        {
+          id: 4,
+          status: 0,
+        },
+        {
+          id: 5,
+          status: 0,
+        }
+      ]
+    },
+    {
+      date: '2018-05-15',
+      attendances: [
+        {
+          id: 1,
+          status: 0,
+        },
+        {
+          id: 2,
+          status: 0,
+        },
+        {
+          id: 3,
+          status: 0,
+        },
+        {
+          id: 4,
+          status: 0,
+        },
+        {
+          id: 5,
+          status: 0,
+        }
+      ]
+    },
+    {
+      date: '2018-05-16',
+      attendances: [
+        {
+          id: 1,
+          status: 0,
+        },
+        {
+          id: 2,
+          status: 0,
+        },
+        {
+          id: 3,
+          status: 0,
+        },
+        {
+          id: 4,
+          status: 0,
+        },
+        {
+          id: 5,
+          status: 0,
+        }
+      ]
+    },
+    {
+      date: '2018-05-17',
+      attendances: [
+        {
+          id: 1,
+          status: 0,
+        },
+        {
+          id: 2,
+          status: 0,
+        },
+        {
+          id: 3,
+          status: 0,
+        },
+        {
+          id: 4,
+          status: 0,
+        },
+        {
+          id: 5,
+          status: 0,
+        }
+      ]
+    },
+    {
+      date: '2018-05-18',
+      attendances: [
+        {
+          id: 1,
+          status: 0,
+        },
+        {
+          id: 2,
+          status: 0,
+        },
+        {
+          id: 3,
+          status: 0,
+        },
+        {
+          id: 4,
+          status: 0,
+        },
+        {
+          id: 5,
+          status: 0,
+        }
+      ]
+    },
+    {
+      date: '2018-05-19',
+      attendances: [
+        {
+          id: 1,
+          status: 0,
+        },
+        {
+          id: 2,
+          status: 0,
+        },
+        {
+          id: 3,
+          status: 0,
+        },
+        {
+          id: 4,
+          status: 0,
+        },
+        {
+          id: 5,
+          status: 0,
+        }
+      ]
+    },
+    {
+      date: '2018-05-20',
+      attendances: [
+        {
+          id: 1,
+          status: 0,
+        },
+        {
+          id: 2,
+          status: 0,
+        },
+        {
+          id: 3,
+          status: 0,
+        },
+        {
+          id: 4,
+          status: 0,
+        },
+        {
+          id: 5,
+          status: 0,
         }
       ]
     }
